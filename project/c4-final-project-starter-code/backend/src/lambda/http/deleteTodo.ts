6ae1dfeb229c21d6b0db71f2f907d47028b8d264
Chisom Nnabuihe <chisomnnabuihe@gmail.com>
2022-12-05 14:34:49 +0000

    Todo

--- project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -3,17 +3,28 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { deleteTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+import { deleteTodo } from '../../helpers/todos'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Remove a TODO item by id
-    
-    return undefined
+  const user = getUserId(event)
+  const item = await deleteTodoItem(todoId, user)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      item
+    })
   }
+}
 )
 
 handler
--- project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { updateTodo } from '../../businessLogic/todos'
+import { updateTodo } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
 
@@ -13,10 +13,22 @@ export const handler = middy(
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+ try {
+    await updateTodo(userId, todoId, updatedTodo)
 
-
-    return undefined
-)
+    return {
+      statusCode: 201,
+      body: ''
+    }
+  } catch (error) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error
+      })
+    }
+  }
+})
 
 handler
   .use(httpErrorHandler())
